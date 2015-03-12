cf00ee0955ff6368ab20bb82b226dbc5c8c4ca69
bjarkihall <bjarkihall@hotmail.com>
2015-03-12 17:48:55 +0000

    no getelementbyid, fixed a search error

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,77 +1,75 @@
-// ===========
-// Index CLASS
-// ===========
-var index = {
-	
-	query: $("#searchString"),
+$(document).ready(function(){
+	// ===========
+	// Index CLASS
+	// ===========
+	var index = {
 
-	init: function(){
-		
-		// =================
-		// Search dropdown button
-		// =================
+		query: $("#searchString"),
 
-		$(".dropdown-menu li a").click(function(){
-			var selText = $(this).text();
-			$(this).parents('.input-group-btn').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-		});
+		init: function(){
+			// =================
+			// Search dropdown button
+			// =================
 
-		// =================
-		// User text input HANDLING
-		// =================
+			$(".dropdown-menu li a").click(function(){
+				var selText = $(this).text();
+				$(this).parents('.input-group-btn').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+			});
 
-		document.addEventListener("DOMContentLoaded", function(){
-			$("#searchForm").addEventListener("submit", function(e){
+			// =================
+			// User text input HANDLING
+			// =================
+			$("#searchForm").submit(function(e){
 				e.preventDefault();
 				query = $("#searchString");
 				search.searchQuery(query);
-			}, false);
-		});
-	},
-	
-	update: function(){
-		// Get the newest stuff from both databases and call display
-	},
-	
-	display: function(){
-		// Display most recent programmes obtained from databases
-	},
-	
-	sortByTitle: function(){
-		// Self-explaining
-	},
-	
-	sortByDate: function(){
-		// Self-explaining
-	}
-}
-
-index.init();
-
-// ============
-// Search CLASS
-// ============
-var search = {
-	
-	searchQuery: function(query){
-		if(query[0].value!==""){
-			// Tell user what has been searched
-			alert('You searched for: \"'+query[0].value+'\", while choosing: \"'+$('#searchForm .btn').text().trim()+'\".');
-		}
+			});
+		},
 		
-		/* AJAX CODE:
-		// We need to be running a server for this to work
-		var xmlhttp;
-		if (window.XMLHttpRequest) xmlhttp=new XMLHttpRequest();
-		else xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		xmlhttp.onreadystatechange=function() {
-			if (xmlhttp.readyState==4 && xmlhttp.status==200){
-				$("#output").innerHTML = xmlhttp.responseText;
-				alert("You searched for: "+query[0].value+""+$('#searchForm .btn').text());
-			}
+		update: function(){
+			// Get the newest stuff from both databases and call display
+		},
+		
+		display: function(){
+			// Display most recent programmes obtained from databases
+		},
+		
+		sortByTitle: function(){
+			// Self-explaining
+		},
+		
+		sortByDate: function(){
+			// Self-explaining
 		}
-		xmlhttp.open("POST","phplogic/handleUserInput.php?srch-term="+query.value,true); //this is how it's done in php
-		xmlhttp.send();
-		*/
 	}
-}
+
+	index.init();
+
+	// ============
+	// Search CLASS
+	// ============
+	var search = {
+		
+		searchQuery: function(query){
+			if(query[0].value!==""){
+				// Tell user what has been searched
+				alert('You searched for: \"'+query[0].value+'\", while choosing: \"'+$('#searchForm .btn').text().trim()+'\".');
+			}
+			
+			/* AJAX CODE:
+			// We need to be running a server for this to work
+			var xmlhttp;
+			if (window.XMLHttpRequest) xmlhttp=new XMLHttpRequest();
+			else xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			xmlhttp.onreadystatechange=function() {
+				if (xmlhttp.readyState==4 && xmlhttp.status==200){
+					$("#output").innerHTML = xmlhttp.responseText;
+					alert("You searched for: "+query[0].value+""+$('#searchForm .btn').text());
+				}
+			}
+			xmlhttp.open("POST","phplogic/handleUserInput.php?srch-term="+query.value,true); //this is how it's done in php
+			xmlhttp.send();
+			*/
+		}
+	}
+});
