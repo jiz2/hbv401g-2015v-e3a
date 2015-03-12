63c4567b1874b99f8be723ba8f840387885882b9
jiz2 <jiz2@hi.is>
2015-03-12 17:31:13 +0000

    more javafication and fitting our class diagram

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,9 @@
-// ===================
-// Example Index CLASS
-// ===================
+// ===========
+// Index CLASS
+// ===========
 var index = {
 	
-	searchForm: document.getElementById("searchForm"),
+	query: $("#searchString"),
 
 	init: function(){
 		
@@ -21,32 +21,44 @@ var index = {
 		// =================
 
 		document.addEventListener("DOMContentLoaded", function(){
-			searchForm.addEventListener("submit", index.add, false);
+			$("#searchForm").addEventListener("submit", function(e){
+				e.preventDefault();
+				query = $("#searchString");
+				search.searchQuery(query);
+			}, false);
 		});
 	},
 	
-	add: function(e){
-		e.preventDefault();
-		index.postSearch();
+	update: function(){
+		// Get the newest stuff from both databases and call display
 	},
-
-	postSearch: function(){
-		var str = $("#searchString");
-		if(str[0].value!==""){
-			alert('You searched for: \"'+str[0].value+'\", while choosing: \"'+$('#searchForm .btn').text().trim()+'\".');
-			search.searchQuery(str);
-		}
+	
+	display: function(){
+		// Display most recent programmes obtained from databases
+	},
+	
+	sortByTitle: function(){
+		// Self-explaining
+	},
+	
+	sortByDate: function(){
+		// Self-explaining
 	}
 }
 
 index.init();
 
-// ====================
-// Example Search CLASS
-// ====================
+// ============
+// Search CLASS
+// ============
 var search = {
 	
-	searchQuery: function(str){
+	searchQuery: function(query){
+		if(query[0].value!==""){
+			// Tell user what has been searched
+			alert('You searched for: \"'+query[0].value+'\", while choosing: \"'+$('#searchForm .btn').text().trim()+'\".');
+		}
+		
 		/* AJAX CODE:
 		// We need to be running a server for this to work
 		var xmlhttp;
@@ -55,10 +67,10 @@ var search = {
 		xmlhttp.onreadystatechange=function() {
 			if (xmlhttp.readyState==4 && xmlhttp.status==200){
 				$("#output").innerHTML = xmlhttp.responseText;
-				alert("You searched for: "+str[0].value+""+$('#searchForm .btn').text());
+				alert("You searched for: "+query[0].value+""+$('#searchForm .btn').text());
 			}
 		}
-		xmlhttp.open("POST","phplogic/handleUserInput.php?srch-term="+str.value,true); //this is how it's done in php
+		xmlhttp.open("POST","phplogic/handleUserInput.php?srch-term="+query.value,true); //this is how it's done in php
 		xmlhttp.send();
 		*/
 	}
