94fc6aa399e1ca05b54bcf9bb032817fb6f1a6d5
jiz2 <jiz2@hi.is>
2015-03-14 20:09:13 +0000

    migrate to OOP, one test object is in! under public

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,10 +12,10 @@ var index = {
 	query: $("#searchInput").val(),
 	
 	init: function(){
-		createRow();
-		ajaxTesting();
-
-		// =================
+		// =====================
+		// Initialize JavaScript
+		// =====================
+		
 		// Search dropdown button
 		// =================
 		$(".dropdown-menu li a").click(function(){
@@ -23,12 +23,11 @@ var index = {
 			$(this).parents('.input-group-btn').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
 		});
 
-		// =================
-		// User text input HANDLING
-		// =================
+		// User text input Handling
+		// ========================
 		$("#searchForm").submit(function(e){
 			e.preventDefault();
-			query = $("#searchInput").val();
+			index.query = $("#searchInput").val();
 			search.searchQuery(index.query);
 		});
 		
@@ -39,31 +38,72 @@ var index = {
 				alert('Your email is: \"' + query + '\".');
 		});
 
+		// Display Type Handling
+		// =====================
 		$("button#moreRows").click(function(){
-			createRow();
-			outputConcert(g_res);
+			index.addRows();
+			index.display();
 		});
+		
+		$('#list').click(function(event){
+			event.preventDefault();
+			$('.concertDisplay .col-xs-12').addClass('list-group-item');
+		});
+		
+		$('#grid').click(function(event){
+			event.preventDefault();
+			$('.concertDisplay .col-xs-12').removeClass('list-group-item');
+		});
+		
+		// Initialize default results
+		this.addRows();
+		this.update();
 	},
 	
-	update: function(result){
+	update: function(){
 		// Get the newest stuff from both databases and call display
-		if(result) {
-			this.display(result);
-		} else alert("No result!");
+		search.searchQuery();
 	},
 	
-	display: function(result){
+	display: function(test){
 		// Display most recent programmes obtained from databases
-		if(result.programmes) {
+		if(test){
 			var res = result.programmes;
+			console.log(res);
+			var colImg = $('.col-md-4 img')
+			var colH2 = $('.col-md-4 h2');
+			var colPDet = $('.col-md-4 p.details');
+			for(var i = 0; i < res.length; i++){
+				colImg.eq(i).attr({
+					src: '',
+					alt: ''
+				});
+				colH2.eq(i).text(res[i].title);
+				colPDet.eq(i).html(
+					'Series: \"' + res[i].seriesNo + '" Episode: \"' + res[i].epNo
+					+ '\"<br>Start Time: \"' + res[i].startTime
+					+ '\"<br>Duration: \"' + res[i].duration
+					+ '\"<br>Channel: \"' 	+ res[i].channel 
+					+ '\"<br>Numbur of Downloads: \"' + res[i].downloadNo
+					+ '\"<br>Ratings: \"' + res[i].avgRatings
+					+ '\"Rated by: \"' + res[i].ratingNo + '\".'
+					+ '\"<br><br>Programme ID: \"' + res[i].id
+				);
+			}
+		} else {
+			var res = result.programmes;
+			
+			var colImg = $('.col-md-4 img')
 			var colH2 = $('.col-md-4 h2');
 			var colPDet = $('.col-md-4 p.details');
 			for(var i = 0; i < colH2.length; i++){
+				colImg.eq(i).attr({
+					src: res[i].imageSource,
+					alt: 'Pic of ' + res[i].eventDateName
+				});
 				colH2.eq(i).text(res[i].eventDateName);
 				colPDet.eq(i).html(
-					'<img src="' + res[i].imageSource
-					+ '" alt="' + 'Pic of ' + res[i].eventDateName + '">'
-					+ '<br>Name: \"' 						+ res[i].name
+					'Name: \"' 				+ res[i].name
 					+ '\"<br>DateOfShow: \"' 		+ res[i].dateOfShow
 					+ '\"<br>UserGroupName: \"' 	+ res[i].userGroupName
 					+ '\"<br>EventHallName: \"' 	+ res[i].eventHallName + '\".'
@@ -78,6 +118,41 @@ var index = {
 	
 	sortByDate: function(){
 		// Self-explaining
+	},
+	
+	addRows: function() {
+		// Creates HTML container for programme display
+		var nrOfCols = 3; // Default number of columns
+		var nrOfRows = 2; // Default number of rows
+		
+		for(var i = 0; i < nrOfRows; i++){
+			// Only add more events if they exist in the array
+			var diff = result.programmes.length - $('.concertDisplay .col-md-4').length;
+			if(diff < nrOfCols)
+				if(result.programmes.length > 0)
+					if(diff === 0)
+						return;
+					else
+						nrOfCols = diff;
+			
+			// Generate corresponding HTML code
+			var str = "";
+			for(var j = 0; j < nrOfCols; j++){
+				str += '<div class="col-xs-12 col-md-4">'
+					+'<img class="resultImg img-responsive"></img>'
+					+'<h2></h2>'
+					+'<p class="details"></p>'
+					+'<p class="viewMore">'
+						+'<a class="detailsButton btn btn-default" href="#" role="button">'
+							+'View details &raquo;'
+						+'</a>'
+					+'</p>'
+				+'</div>';
+			}
+			
+			// Attach the HTML code
+			$('.concertDisplay button#moreRows').before('<div class="row">'+str+'</div>');
+		}
 	}
 }
 
@@ -87,11 +162,22 @@ var index = {
 var search = {
 	
 	searchQuery: function(query){
-		if(query !== ""){
+		if(query && query === "test"){
+			//Showing off the Ajax power
+			console.log("testing mock object");
+			$.ajax({
+				'url': './mockObjects',
+				'type': 'GET',
+				'dataType': 'json',
+				'success': function(response) {
+					result.programmes = response.results;
+					index.display(query);
+				}
+			});
+		} else if(query && query !== ""){
 			// Tell user what has been searched
-			alert('You searched for: \"' + query + '\", while choosing: \"'
+			console.log('You searched for: \"' + query + '\", while choosing: \"'
 				+ $('#searchForm .btn').text().trim() + '\".');
-			ajaxTesting();
 
 			//Showing off the Ajax power
 			$.ajax({
@@ -99,94 +185,39 @@ var search = {
 				'type': 'GET',
 				'dataType': 'json',
 				'success': function(response) {
-					index.update(new Result(response.results));
+					result.programmes = response.results;
+					index.display();
+				}
+			});
+		} else {
+			// Default search
+			$.ajax({
+				'url': 'http://apis.is/concerts',
+				'type': 'GET',
+				'dataType': 'json',
+				'success': function(response) {
+					result.programmes = response.results;
+					index.display();
 				}
 			});
 		}
 	}
 }
 
-// ===============================
-// Result CLASS (Constructor Only)
-// ===============================
-function Result(programmes, dlId, seats, bookNr) {
+// ============
+// Result CLASS
+// ============
+var result = {
 
 	// Search results
-	if (programmes) this.programmes = programmes;
+	programmes: [],
 	
 	// A TV download booking ID returned by TV Database Engine
-	if (dlId) this.dlId = dlId;
+	dlId: undefined,
 	
 	// List of seats available when booking for a Concert
-	if (seats) this.seats = seats;
+	seats: undefined,
 	
 	// Booking number of a concert returned by Concert Database Engine
-	if (bookNr) this.bookNr = bookNr;
+	bookNr: undefined
 }
-
-function outputConcert(res){
-	var colImg = $('.col-md-4 img')
-	var colH2 = $('.col-md-4 h2');
-	var colPDet = $('.col-md-4 p.details');
-	for(var i = 0; i < colH2.length; i++){
-		colImg.eq(i).attr({
-			src: res[i].imageSource,
-			alt: 'Pic of' + res[i].eventDateName
-		});
-		colH2.eq(i).text(res[i].eventDateName);
-		colPDet.eq(i).html(
-			'Name: \"' 				+ res[i].name
-			+ '\"<br>DateOfShow: \"' 		+ res[i].dateOfShow
-			+ '\"<br>UserGroupName: \"' 	+ res[i].userGroupName
-			+ '\"<br>EventHallName: \"' 	+ res[i].eventHallName + '\".'
-		);
-	}
-}
-
-function ajaxTesting(){
-	//Showing off the Ajax power
-	$.ajax({
-		'url': 'http://apis.is/concerts',
-		'type': 'GET',
-		'dataType': 'json',
-		'success': function(response) {
-			console.log(response);
-			var res = response.results;
-			g_res = res; //temp solution!
-			outputConcert(res);
-		}
-	});
-}
-
-function createRow(){
-	var nrOfCols = 3;
-	var nrOfRows = 2;
-	for(var i = 0; i < nrOfRows; i++){
-		var diff = g_res.length - $('.concertDisplay .col-md-4').length;
-		// Only add more events if they exist in the array
-		if(diff < nrOfCols)
-			if(g_res.length > 0)
-				if(diff === 0)
-					return;
-				else
-					nrOfCols = diff;
-		var str = "";
-		for(var j = 0; j < nrOfCols; j++){
-			str += '<div class="col-xs-12 col-md-4">'
-				+'<img class="resultImg img-responsive"></img>'
-				+'<h2></h2>'
-				+'<p class="details"></p>'
-				+'<p class="viewMore">'
-					+'<a class="detailsButton btn btn-default" href="#" role="button">'
-						+'View details &raquo;'
-					+'</a>'
-				+'</p>'
-			+'</div>';
-		}
-		$('.concertDisplay button#moreRows').before('<div class="row">'+str+'</div>');
-	}
-}
-
-
-$('#list').click(function(event){event.preventDefault();$('.concertDisplay .col-xs-12').addClass('list-group-item');});
-$('#grid').click(function(event){event.preventDefault();$('.concertDisplay .col-xs-12').removeClass('list-group-item');});
