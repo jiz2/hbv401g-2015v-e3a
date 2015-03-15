fc68d11cbda9ba7e5468cb55d3a0fefc89b4fb55
bjarkihall <bjarkihall@hotmail.com>
2015-03-15 22:41:09 +0000

    cleaning search.js

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -4,49 +4,53 @@
 var search = {
 	
 	searchQuery: function(query){
+		// User must be searching for something
+		if(!query || query==="")
+			return;
+		else
+			query = query.trim();
+
 		var category = $('#searchForm .btn').text().trim();
 		var uri = "";
-		var test = false;
-		if(query) {
-			var query = query.trim();
-			if(query === "test") {
-				test = true;
-			}
-		}
+		var isTesting = (query === "test");
+
 		if(category === "All") {
-			if(test)
+			if(isTesting)
 				uri = './mockObjects';
 			else
 				category = "Concert";
 		}
 		if(category === "Concert") {
-			if(test)
+			if(isTesting)
 				uri = './mockConcert';
 			else
 				uri = 'http://apis.is/concerts';
 		}
 		if(category === "TV program") {
-			if(test)
+			if(isTesting)
 				uri = './mockTV';
 			else
 				uri = 'http://apis.is/tv/ruv';
 		}
-		if(test) {
+		if(isTesting) {
 			console.log("testing mock object");
 		}
 		else {
 			console.log(
-				["Searching for the", category, query+"."]
-				.join(' ')
+				["Searching for the", category, query+"."].join(' ')
 			);
 		}
+		
 		$.ajax({
 			'url': uri,
 			'type': 'GET',
 			'dataType': 'json',
 			'success': function(response) {
 				result.programmes = response.results;
-				index.display(test);
+				if(isTesting)
+						index.display(isTesting);
+				else
+					index.display();
 			}
 		});
 	}
