eb1816b328f25a605aea7b7cb86f346ba34d38f4
Bjartur Thorlacius <bjt9@hi.is>
2015-03-15 16:37:44 +0000

    Auka æfingarpróf fyrir indexOf

--- test/test.js
+++ test/test.js
@@ -1,8 +1,9 @@
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
+      [1,2,4].indexOf(5).should.equal(-1);
+      [1,2,4].indexOf(3).should.equal(-1);
+      [1,2,4].indexOf(0).should.equal(-1);
     })
   })
 })
