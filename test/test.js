2eaede5444fc3785c15dc3159a99b820a71242b7
Bjartur Thorlacius <bjt9@hi.is>
2015-03-15 17:24:25 +0000

    State dependencies in test/test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
+"use strict";
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+chai.should();
+var expect = chai.expect;
+chai.use(sinonChai);
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      [1,2,4].indexOf(5).should.equal(-1);
-      [1,2,4].indexOf(3).should.equal(-1);
-      [1,2,4].indexOf(0).should.equal(-1);
-    })
+	  //Test numbers that are indeed not members ef [1,2,4]
+	  [0,3,5].map(function(n) {
+		  [1,2,4].indexOf(n).should.equal(-1);
+      });
+    });
   })
 })
