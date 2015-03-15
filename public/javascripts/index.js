b04c8f46f952145be30b23390c1118c912cb6543
Bjartur Thorlacius <bjt9@hi.is>
2015-03-15 21:18:05 +0000

    Revert "Uppfæra leitarniðurstöður þegar skipt er á milli TV og Concerts"
    This reverts commit 73ec9c04bd3a1738cabc0deacad3bb07a2c5c361.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,7 +15,6 @@ var index = {
 		$(".dropdown-menu li a").click(function(){
 			var selText = $(this).text();
 			$(this).parents('.input-group-btn').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
-			$("#searchForm").submit();
 		});
 
 		// User text input Handling
