4946284c20d7d06d51f93d2e9ddd80fe239561f7
jiz2 <jiz2@hi.is>
2015-04-12 13:39:50 +0000

    divided searchQuery

--- public/javascripts/objects/Search.js
+++ public/javascripts/objects/Search.js
@@ -12,33 +12,32 @@ var Search = {
 		var category = $('#searchForm .btn').text().trim();
 		var uri = "";
 
-		switch(category) {
-			case "Concert": 
-				uri = 'http://apis.is/concerts';
-				break;
-			case "TV program":
-				uri = 'http://apis.is/tv/ruv';
-				break;
-			default: // default is case All
-				uri = '/process'
+		// Search for Concerts
+		if(category === "All" || category === "Concert"){
+			uri = 'http://apis.is/concerts';
+		}
+		
+		// Search for TV programs
+		if(category === "All" || category === "TV program"){
+			uri = '/process'
+			
+			var date = new Date(); // what is this? don't we use startDate/endDate?
+			
+			// Get results for TV
+			$.ajax({
+				'url': uri,
+				'type': 'GET',
+				'data': {title: query, stations: 'ruv', date: '2015-04-12'},
+				'dataType': 'json',
+				'success': function(response) {
+					Search.results[0] = response;
+					MainSite.displayResults();
+				}
+			});
 		}
 
 		console.log(
-			["Searching for the category:", category, query+"."].join(' ')
+			["Searching for", query, "in", category].join(' ')
 		);
-		
-		var date = new Date(); // what is this? don't we use startDate/endDate?
-		
-		// Get results for TV
-		$.ajax({
-			'url': uri,
-			'type': 'GET',
-			'data': {title: query, stations: 'ruv', date: '2015-04-12'},
-			'dataType': 'json',
-			'success': function(response) {
-				Search.results[0] = response;
-				MainSite.displayResults();
-			}
-		});
 	}
 }
