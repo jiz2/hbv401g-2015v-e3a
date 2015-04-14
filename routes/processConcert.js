c81824faae1d181b0efbb2131b64f2fad39f5fcf
jiz2 <jiz2@hi.is>
2015-04-14 18:37:29 +0000

    added search input for concert

--- routes/processConcert.js
+++ routes/processConcert.js
@@ -3,11 +3,10 @@ var router = express.Router();
 var concert = require('../search');
 
 router.get('/', function(req, res, next) {
-  var title = req.query['title'];
-  var station = req.query['stations'];
+  var term = req.query['term'];
   var date = req.query['date'];
-  console.log(title,station,date,"we are prcessing! Please be patient :*");
-  var testCriteria = {title: title, station: station, date: date};
+  console.log(term,date,"we are prcessing! Please be patient :*");
+  var testCriteria = {term: term, date: date};
 
   var doneFetching = function(response){
     var results = JSON.stringify(response);
