48a72314704d473866dbb87e4da93304e467f987
jiz2 <jiz2@hi.is>
2015-04-14 18:25:22 +0000

    app

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var process = require('./routes/process');
+var processTV = require('./routes/processTV');
+var processConcert = require('./routes/processConcert');
 
 var app = express();
 // view engine setup
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/process', process);
+app.use('/processTV', processTV);
+app.use('/processConcert', processConcert);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
